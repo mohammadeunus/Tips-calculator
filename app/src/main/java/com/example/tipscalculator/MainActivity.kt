67c7014c0a037a74573f0262d8aa4d86b589a39f
mohammad eunus <38430781+mohammadeunus@users.noreply.github.com>
2021-07-31 14:33:38 +0600

    aa

--- app/src/main/java/com/example/tipscalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipscalculator/MainActivity.kt
@@ -1,9 +1,8 @@
-package com.example.tiptime
+package com.example.tipscalculator
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.tipscalculator.R
-import com.example.tiptime.databinding.ActivityMainBinding
+import com.example.tipscalculator.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
@@ -14,31 +13,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.calculateButton.setOnClickListener { calculateTip() }
+        setContentView(R.layout.activity_main)
+        binding.mainActivityCalculateButton.setOnClickListener{ calculateTip() }
     }
 
     private fun calculateTip() {
-        val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDoubleOrNull()
-        if (cost == null) {
-            binding.tipResult.text = ""
-            return
-        }
+        val stringInTextField = binding.mainActivityCostOfService.text.toString()
+        val cost = stringInTextField.toDouble()
 
-        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
-            R.id.option_twenty_percent -> 0.20
-            R.id.option_eighteen_percent -> 0.18
-            else -> 0.15
+        val tipPercentage = when (binding.mainActivityTipOptions.checkedRadioButtonId) {
+            R.id.mainActivity_option_eight_percent -> 0.08
+            R.id.mainActivity_option_fifteen_percent -> 0.15
+            else -> 0.20
         }
-
-        var tip = tipPercentage * cost
-        if (binding.roundUpSwitch.isChecked) {
+        var tip= tipPercentage * cost
+        if(binding.mainActivityRoundUpSwitch.isChecked) {
             tip = kotlin.math.ceil(tip)
         }
 
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
-        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+        binding.mainActivityTipResult.text = getString(R.string.mainActivity_tip_amount, formattedTip)
     }
+
+
 }
